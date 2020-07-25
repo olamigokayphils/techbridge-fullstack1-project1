d8b8187a0e90604588c0bf5347d736fd45dfebbb
Olamigoke Philip <olamigokayphils@gmail.com>
2020-07-25 12:53:10 +0100

    Initial Commit for Todo's Application

---
+++ control.js
@@ -0,0 +1,42 @@
+// //console.dir(document);
+
+// console.log(document.URL);
+// console.log(document.domain);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.links);
+// console.log(document.images)
+
+const listItems = document.getElementsByClassName("list-group-item");
+// listItems[0].textContent = "Play some games";
+// listItems[0].style.fontWeight = "bold";
+// listItems[0].style.backgroundColor = "#d66f6f";
+// listItems[0].style.color = "#ffffff";
+
+// for (let i = 0; i < listItems.length; i++) {
+//   listItems[i].style.backgroundColor = "#d66f6f";
+// }
+
+// let addActivity = document.getElementById("add-activity");
+// addActivity.textContent = "Done Boss";
+
+// let allListItems = document.getElementsByTagName("li");
+// console.log(allListItems);
+
+//QUERY SELECTOR
+
+// let listGroup = document.querySelector(".list-group");
+// console.log(listGroup);
+
+// listGroup.style.fontWeight = "700";
+// listGroup.style.color = "green";
+
+// let button = document.querySelector("input[type='submit']");
+// let numbersOfLikes = 0;
+// button.value = "Likes";
+// button.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   numbersOfLikes += 1;
+//   button.value = `Likes (${numbersOfLikes})`;
+// });
