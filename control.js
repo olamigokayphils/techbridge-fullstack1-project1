e70a13d1ab1be5910de332c9abb26def2ad4bb31
Olamigoke Philip <olamigokayphils@gmail.com>
2020-07-26 17:08:26 +0100

    July 26 Class

--- control.js
+++ control.js
@@ -8,7 +8,7 @@
 // console.log(document.links);
 // console.log(document.images)
 
-const listItems = document.getElementsByClassName("list-group-item");
+//const listItems = document.getElementsByClassName("list-group-item");
 // listItems[0].textContent = "Play some games";
 // listItems[0].style.fontWeight = "bold";
 // listItems[0].style.backgroundColor = "#d66f6f";
@@ -40,3 +40,92 @@ const listItems = document.getElementsByClassName("list-group-item");
 //   numbersOfLikes += 1;
 //   button.value = `Likes (${numbersOfLikes})`;
 // });
+
+// JUL 26
+let activityList = [
+  {
+    title: "Wash the dishes",
+    finished: false,
+  },
+  {
+    title: "Cook",
+    finished: false,
+  },
+  {
+    title: "Mow the lawn",
+    finished: false,
+  },
+  {
+    title: "Finish up my Assignment",
+    finished: false,
+  },
+  {
+    title: "Take a nap",
+    finished: false,
+  },
+  {
+    title: "Pick Debbie from school",
+    finished: false,
+  },
+];
+
+function deleteActivityFromArray() {
+  window.alert("Delete");
+}
+
+const activityListParentElement = document.querySelector("#activity-list");
+
+function mapArrayToDOM() {
+  activityListParentElement.innerHTML = "";
+  activityList.forEach(function (activity, index) {
+    // CREATE CHILD ELEMENT
+    const newActivityDom = document.createElement("li");
+    newActivityDom.className = "list-group-item";
+
+    //CREATE INNER CHILD DIV
+    const activityItemDiv = document.createElement("div");
+    activityItemDiv.className = "activity-item";
+
+    // CREATE TITLE SPAN
+    const titleSpan = document.createElement("span");
+    titleSpan.textContent = activity.title;
+
+    // CREATE DELETE BUTTON
+    const deleteActivityButton = document.createElement("button");
+    deleteActivityButton.className = "btn btn-danger";
+    deleteActivityButton.type = "button";
+    deleteActivityButton.textContent = "Delete";
+    deleteActivityButton.onclick = `${deleteActivityFromArray()}`;
+
+    //APPEND TITLE SPAN & DELETE BUTTON TO 'INNER CHILD DIV'
+    activityItemDiv.appendChild(titleSpan);
+    activityItemDiv.appendChild(deleteActivityButton);
+
+    //APPEND INNER CHILD DIV TO ACTIVITY CHILD ELEMENT
+    newActivityDom.appendChild(activityItemDiv);
+
+    // APPEND CHILD TO PARENT ELEMENT
+    activityListParentElement.appendChild(newActivityDom);
+  });
+}
+
+mapArrayToDOM();
+
+const submitButton = document.querySelector("input[type='submit']");
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  const textInputValue = document.querySelector("input[type='text']").value;
+
+  if (textInputValue == "") {
+    window.alert("Event cannot be empty.");
+  } else {
+    activityList.push({
+      title: textInputValue,
+      finished: false,
+    });
+
+    mapArrayToDOM();
+
+    document.querySelector("form").reset();
+  }
+});
